4f69036a4b7cb823f8d08e4c629926aa0ea2262e
jjsznaniszlo <johnszaniszlo@gmail.com>
2024-08-16 23:06:54 -0700

    fix formatting of hyprland.nix

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -1,17 +1,18 @@
 { pkgs, lib, ...}:
 let
-  startup-script = pkgs.pkgs.writeShellScriptBin "start" '''';
+	startup-script = pkgs.pkgs.writeShellScriptBin "start" '''';
 in
 {
 	wayland.windowManager.hyprland = {
+		enable = true;
 		settings = {
 			monitor = [
 				"DP-1,3840x2160@120,0x0,1"
 				"DP-3,2560x1440@165,3840x0,1,transform,3"
-      ];
+			];
 			"$terminal" = "wezterm";
 			"$menu" = "rofi -show drun -show-icons";
-      exec-once = [
+			exec-once = [
 				"eww open & hyprpaper"
 				''${startup-script}/bin/start''
 			];
@@ -19,16 +20,16 @@ in
 				"XCURSOR_SIZE,24"
 				"HYPRCURSOR_SIZE,24"
 			];
-      general = {
+			general = {
 				gaps_in = 5;
-        gaps_out = 20;
-        border_size = 2;
+				gaps_out = 20;
+        			border_size = 2;
 				resize_on_border = false;
 				allow_tearing = false;
 				layout = "dwindle";
 			};
 			decoration = {
-    		rounding = 10;
+				rounding = 10;
 				active_opacity = 1.0;
 				inactive_opacity = 1.0;
 
@@ -80,58 +81,58 @@ in
 				};
 			};
 			gestures = {
-			  workspace_swipe = false;
+				workspace_swipe = false;
 			};
 			"$mainMod" = "SUPER";
 			bind = [
 				"$mainMod, Q, exec, $terminal"
 				"$mainMod, C, killactive,"
-			  "$mainMod, M, exit,"
-			  "$mainMod, V, togglefloating,"
+			  	"$mainMod, M, exit,"
+			  	"$mainMod, V, togglefloating,"
 				"$mainMod, R, exec, $menu"
 				"$mainMod, P, pseudo,"
-			  "$mainMod, J, togglesplit,"
+				"$mainMod, J, togglesplit,"
 
-			  "$mainMod, left, movefocus, l"
-   			"$mainMod, right, movefocus, r"
-			  "$mainMod, up, movefocus, u"
-			  "$mainMod, down, movefocus, d"
+				"$mainMod, left, movefocus, l"
+				"$mainMod, right, movefocus, r"
+				"$mainMod, up, movefocus, u"
+				"$mainMod, down, movefocus, d"
 
-			  "$mainMod, 1, workspace, 1"
-			  "$mainMod, 2, workspace, 2"
-			  "$mainMod, 3, workspace, 3"
-			  "$mainMod, 4, workspace, 4"
-			  "$mainMod, 5, workspace, 5"
-			  "$mainMod, 6, workspace, 6"
-			  "$mainMod, 7, workspace, 7"
-			  "$mainMod, 8, workspace, 8"
-			  "$mainMod, 9, workspace, 9"
-			  "$mainMod, 0, workspace, 10"
+				"$mainMod, 1, workspace, 1"
+				"$mainMod, 2, workspace, 2"
+				"$mainMod, 3, workspace, 3"
+				"$mainMod, 4, workspace, 4"
+				"$mainMod, 5, workspace, 5"
+				"$mainMod, 6, workspace, 6"
+				"$mainMod, 7, workspace, 7"
+				"$mainMod, 8, workspace, 8"
+				"$mainMod, 9, workspace, 9"
+				"$mainMod, 0, workspace, 10"
 
-			  "$mainMod SHIFT, 1, movetoworkspace, 1"
-			  "$mainMod SHIFT, 2, movetoworkspace, 2"
-			  "$mainMod SHIFT, 3, movetoworkspace, 3"
-			  "$mainMod SHIFT, 4, movetoworkspace, 4"
-			  "$mainMod SHIFT, 5, movetoworkspace, 5"
-			  "$mainMod SHIFT, 6, movetoworkspace, 6"
-			  "$mainMod SHIFT, 7, movetoworkspace, 7"
-			  "$mainMod SHIFT, 8, movetoworkspace, 8"
-			  "$mainMod SHIFT, 9, movetoworkspace, 9"
-			  "$mainMod SHIFT, 0, movetoworkspace, 10"
+				"$mainMod SHIFT, 1, movetoworkspace, 1"
+				"$mainMod SHIFT, 2, movetoworkspace, 2"
+				"$mainMod SHIFT, 3, movetoworkspace, 3"
+				"$mainMod SHIFT, 4, movetoworkspace, 4"
+				"$mainMod SHIFT, 5, movetoworkspace, 5"
+				"$mainMod SHIFT, 6, movetoworkspace, 6"
+			  	"$mainMod SHIFT, 7, movetoworkspace, 7"
+			  	"$mainMod SHIFT, 8, movetoworkspace, 8"
+			  	"$mainMod SHIFT, 9, movetoworkspace, 9"
+			  	"$mainMod SHIFT, 0, movetoworkspace, 10"
 
-			  "$mainMod, S, togglespecialworkspace, magic"
-			  "$mainMod SHIFT, S, movetoworkspace, special:magic"
+			  	"$mainMod, S, togglespecialworkspace, magic"
+			  	"$mainMod SHIFT, S, movetoworkspace, special:magic"
 
-			  "$mainMod, mouse_down, workspace, e+1"
-			  "$mainMod, mouse_up, workspace, e-1"
+			  	"$mainMod, mouse_down, workspace, e+1"
+			  	"$mainMod, mouse_up, workspace, e-1"
 				", xf86audioraisevolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%+"
 				", xf86audiolowervolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
-    	];
+    			];
 			bindm = [
 				"$mainMod, mouse:272, movewindow"
 				"$mainMod, mouse:273, resizewindow"
 			];
 			windowrulev2 = "suppressevent maximize, class:.*";
 		};
-  };
+	};
 }
