fcd5d2bcd017a175a678a3e005d9131fef047385
jjsznaniszlo <johnszaniszlo@gmail.com>
2024-08-22 00:12:50 -0700

    add openssh

---
+++ hosts/common/global/openssh.nix
@@ -0,0 +1,24 @@
+{outputs, lib, ...} : 
+let
+  hosts = lib.attrNames outputs.nixosConfigurations ++ 
+    lib.attrNames outputs.darwinConfigurations;
+in
+{
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      PermitRootLogin = "no";
+
+      StreamLocalBindUnlink = "yes";
+      GatewayPorts = "clientspecified";
+      AcceptEnv = "WAYLAND_DISPLAY";
+      X11Forwarding = true;
+    };
+  };
+  programs.ssh = {
+    knownHosts = lib.genAttrs hosts (hostname: {
+      publicKeyFile = ../../${hostname}/ssh_host_ed25519_key.pub;
+    });
+  };
+}
