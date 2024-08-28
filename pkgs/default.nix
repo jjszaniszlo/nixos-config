e07d0259670dc5b71ccd43e88ebbbeaad659470e
jjsznaniszlo <johnszaniszlo@gmail.com>
2024-08-28 01:26:27 -0700

    fix path for fetchWallhaven import

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,4 +1,4 @@
 { pkgs ? import <nixpkgs> {}, ... }: rec {
-  fetchWallhaven = pkgs.callPackage ./fetchwallhaven {};
+  fetchWallhaven = pkgs.callPackage ./fetchWallhaven {};
   wallpapers = import ./wallpapers { inherit pkgs fetchWallhaven; };
 }
