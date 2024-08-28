1a902603e0312a0c207c98b39a531adbfea7c8cc
jjsznaniszlo <johnszaniszlo@gmail.com>
2024-08-27 22:46:14 -0700

    change hera to not inherit steam config

--- hosts/hera/configuration.nix
+++ hosts/hera/configuration.nix
@@ -1,10 +1,25 @@
 { config, lib, pkgs, ... }: {
   imports =
     [
-      ../common/global
+      ../common/global/locale.nix
+      ../common/global/nix.nix
+      ../common/global/openssh.nix
+      ../common/global/zsh.nix
       ../common/users/jjszaniszlo
       ./hardware-configuration.nix
     ];
+
+  nixpkgs = {
+    overlays = [];
+    config = {
+      allowUnfree = true;
+    };
+  };
+
+  users.users.admin = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" ];
+  };
 
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
