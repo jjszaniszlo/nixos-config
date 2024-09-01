76b1f67c2daec76af026623367df066f4174a220
jjsznaniszlo <johnszaniszlo@gmail.com>
2024-09-01 03:38:23 -0700

    make ssh available temporarily for hermes

--- hosts/common/global/openssh.nix
+++ hosts/common/global/openssh.nix
@@ -10,13 +10,13 @@ in {
   services.openssh = {
     enable = true;
     settings = {
-      PasswordAuthentication = false;
-      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+      # PermitRootLogin = "no";
 
-      StreamLocalBindUnlink = "yes";
-      GatewayPorts = "clientspecified";
-      AcceptEnv = "WAYLAND_DISPLAY";
-      X11Forwarding = true;
+      # StreamLocalBindUnlink = "yes";
+      # GatewayPorts = "clientspecified";
+      # AcceptEnv = "WAYLAND_DISPLAY";
+      # X11Forwarding = true;
     };
   };
 }
