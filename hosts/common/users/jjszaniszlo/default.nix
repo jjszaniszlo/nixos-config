3163644d3901b7fa4c0ab1b28e2efc5793fa8d46
John Szaniszlo <Johnszaniszlo@gmail.com>
2025-01-12 22:01:27 -0800

    hestia config

--- hosts/common/users/jjszaniszlo/default.nix
+++ hosts/common/users/jjszaniszlo/default.nix
@@ -1,8 +1,13 @@
 {...}: {
   imports = [./base.nix];
+
   users.users.jjszaniszlo = {
     isNormalUser = true;
     home = "/home/jjszaniszlo";
     extraGroups = [ "wheel" ];
   };
+
+  users.users."user".openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINf+Sqev3oqnvNk5ReexB9kwrjJ9Zqj2ae70D/vXmjMi jjszaniszlo"
+  ];
 }
--- hosts/hestia/default.nix
+++ hosts/hestia/default.nix
@@ -10,6 +10,14 @@
     ]
     ++ [inputs.disko.nixosModules.default];
 
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+    };
+  };
+
   networking.hostName = "hestia";
   system.stateVersion = "24.05";
 }
